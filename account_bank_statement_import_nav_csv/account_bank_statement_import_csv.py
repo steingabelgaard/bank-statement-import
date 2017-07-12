b70435f143f5d50b39f7a18ed8b1edbfbab41181
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-07-12 10:13:32 +0200

    typo

--- account_bank_statement_import_nav_csv/account_bank_statement_import_csv.py
+++ account_bank_statement_import_nav_csv/account_bank_statement_import_csv.py
@@ -140,7 +140,7 @@ class AccountBankStatementImport(models.TransientModel):
                     _logger.info("vals_line = %s" % vals_line)
                     transactions.append(vals_line)
                 except Exception as e:
-                    raise UserError(_('Format Error\nLine %d could not be processed\n%s') % (i + 1, ustr(e))
+                    raise UserError(_('Format Error\nLine %d could not be processed\n%s') % (i + 1, ustr(e)))
         except Exception as e:
             raise UserError(_('File parse error:\n%s') % ustr(e))
 
